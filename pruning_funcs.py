e3e54bb47f8d4eac7a7744ee6956e86dc4204cb0
Sumedh Vemuganti <sumedh@Sumedhs-MacBook-Pro.local>
2024-05-10 20:18:40 -0500

    Final Touches

--- pruning_funcs.py
+++ pruning_funcs.py
@@ -3,80 +3,28 @@ from numpy import random
 import torch
 import copy
 
-# Eval metric
+# Sparsity Evaluation Metrics
 # From https://discuss.pytorch.org/t/how-to-count-the-number-of-zero-weights-in-a-pytorch-model/13549/2
 def count_zero_weights(model):
+    '''Count the number of model weights that are zero.'''
     zeros = 0
     for name, param in model.named_parameters():
         if 'weight' in name:
             zeros += param.numel() - param.nonzero().size(0)
     return zeros
-
 def percent_zero_weights(model):
+    '''Compute the percent of model weights that are zero.'''
     num_zeros = count_zero_weights(model)
     total_num_weights = sum(p.numel() for name, p in model.named_parameters() if 'weight' in name)
     return num_zeros / total_num_weights * 100
 
-# Threshold Pruning
-
-def threshold_prune(model, threshold):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if 'weight' in name:
-                mask = torch.abs(param) > threshold
-                param.mul_(mask)
-
-# Laplace-based pruning methods
-
-def laplace_pdf(x: torch.Tensor, scale, loc=0.):
-    return torch.exp(-abs(x-loc)/scale)/(2.*scale)
-
-def normalized_laplace_pdf(x: torch.Tensor, loc=0., scale=0.5):
-    laplace_at = lambda w : np.exp(-abs(w-loc)/scale)/(2.*scale)
-    return (laplace_pdf(x, scale) - laplace_at(1)) / (laplace_at(loc) - laplace_at(1))
-     
-def laplace_prune(model, device, scale=0.5):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if 'weight' in name:
-                laplace_tensor = laplace_pdf(param, scale=scale)
-                mask = torch.bernoulli(1 - laplace_tensor).to(device)
-                param.mul_(mask)
-     
-def normalized_laplace_prune(model, device, scale=0.5):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if 'weight' in name:
-                laplace_tensor = normalized_laplace_pdf(param, scale=scale)
-                mask = torch.bernoulli(1 - laplace_tensor).to(device)
-                param.mul_(mask)
-
-# Percent-based pruning methods
-
-def bernoulli_prune(model, device, probability=0.99):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if 'weight' in name:
-                tensor = torch.bernoulli(torch.full(tuple(param.shape), probability)).to(device)
-                param.mul_(tensor)
-
-def percent_prune_by_layer(model, device, percent=1.5):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if 'weight' in name:
-                num_elems = 1
-                for val in param.shape:
-                    num_elems = num_elems * val
-                flattened = torch.sort(torch.flatten(torch.abs(param)))
-
-                prune_cutoff_idx = int(num_elems*percent/100)
-                prune_cutoff_val = flattened[0][prune_cutoff_idx]
-                mask = torch.abs(param) >= prune_cutoff_val
-
-                param.mul_(mask)
 
 
+################################################################# Algorithm 4 #################################################################
+
+# Baseline Percent Pruning: Algorithm 4 in submitted paper
 def bottom_percent_prune(model, device, percent=1.5):
+    '''Prune the bottom 𝑥% of weightsacross the model'''
     with torch.no_grad():
         all_weights = torch.Tensor().to(device)
         for name, param in model.named_parameters():
@@ -95,7 +43,37 @@ def bottom_percent_prune(model, device, percent=1.5):
                 mask = torch.abs(param) > prune_cutoff_val
                 param.mul_(mask)
 
+#############################################################################################################################################
+
+
+
+################################################################# Algorithm 1 #################################################################
+
+# Percent Pruning by Layer: Algorithm 1 in submitted paper
+def percent_prune_by_layer(model, device, percent=1.5):
+    '''prune the bottom 𝑋 % of weights within each layer'''
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                num_elems = 1
+                for val in param.shape:
+                    num_elems = num_elems * val
+                flattened = torch.sort(torch.flatten(torch.abs(param)))
+                prune_cutoff_idx = int(num_elems*percent/100)
+                prune_cutoff_val = flattened[0][prune_cutoff_idx]
+                mask = torch.abs(param) >= prune_cutoff_val
+                param.mul_(mask)
+
+#############################################################################################################################################
+
+
+
+
+################################################################# Algorithm 2 #################################################################
+
+# Stochastic Percent Pruning: Algorithm 2 in submitted paper
 def percent_prune_with_bernoulli(model, device, percent=5, p_success=0.5, debug=False):
+    '''prune each weight in bottom 𝑋 𝑓 % of weights, with 1/𝑓 probability'''
     with torch.no_grad():
         all_weights = torch.Tensor().to(device)
         for name, param in model.named_parameters():
@@ -123,8 +101,48 @@ def percent_prune_with_bernoulli(model, device, percent=5, p_success=0.5, debug=
                 param.mul_(mask)
                 param += param_copy
 
+#############################################################################################################################################
 
 
+
+################################################################# Algorithm 3 #################################################################
+
+# Laplace Pruning: Algorithm 3 in Paper
+def laplace_pdf(x: torch.Tensor, scale, loc=0.):
+    return torch.exp(-abs(x-loc)/scale)/(2.*scale)
+
+def normalized_laplace_pdf(x: torch.Tensor, loc=0., scale=0.5):
+    laplace_at = lambda w : np.exp(-abs(w-loc)/scale)/(2.*scale)
+    return (laplace_pdf(x, scale) - laplace_at(1)) / (laplace_at(loc) - laplace_at(1))
+     
+def laplace_prune(model, device, scale=0.5):
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                laplace_tensor = laplace_pdf(param, scale=scale)
+                mask = torch.bernoulli(1 - laplace_tensor).to(device)
+                param.mul_(mask)
+
+def normalized_laplace_prune(model, device, scale=0.5):
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                laplace_tensor = normalized_laplace_pdf(param, scale=scale)
+                mask = torch.bernoulli(1 - laplace_tensor).to(device)
+                param.mul_(mask)
+
+#############################################################################################################################################
+
+
+
+
+################################################################# Experimental Unused #################################################################
+def bernoulli_prune(model, device, probability=0.99):
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                tensor = torch.bernoulli(torch.full(tuple(param.shape), probability)).to(device)
+                param.mul_(tensor)
 
 def percent_prune_min_max(model, device, percent=1, debug=False):
     with torch.no_grad():
@@ -156,3 +174,13 @@ def percent_prune_min_max(model, device, percent=1, debug=False):
                     print(mask)
 
                 param.mul_(mask)
+
+# Threshold Pruning Functions
+def threshold_prune(model, threshold):
+    '''Prune all weights less than a threshold t'''
+    with torch.no_grad():
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                mask = torch.abs(param) > threshold
+                param.mul_(mask)
+#############################################################################################################################################
